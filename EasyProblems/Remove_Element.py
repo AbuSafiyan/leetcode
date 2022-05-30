24631c59a93822e42df704f634cd4d452b37fb30
AbuSafiyan <safiyanabu@gmail.com>
2022-05-30 12:07:04 +0530

    Adding Remove_element.py

---
+++ EasyProblems/Remove_Element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums, val):
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count += 1
+        return count
+
+
+nums = [3, 2, 2, 3]
+x = Solution()
+print(x.removeElement(nums))
