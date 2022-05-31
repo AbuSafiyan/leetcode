7e1b865403a1eb7bbad0ba26a9fdd4a84fd4d34a
AbuSafiyan <safiyanabu@gmail.com>
2022-05-31 21:15:14 +0530

    Adding Pascal's_Triangle_II.py file

---
+++ EasyProblems/Pascal's_Triangle_II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getRow(self, rowIndex):
+        result = []
+
+        for i in range(0, rowIndex + 1):
+            result.append([])
+
+            for j in range(0, i + 1):
+                if j == 0:
+                    result[i].append(1)
+                elif i == j:
+                    result[i].append(1)
+                else:
+                    result[i].append((result[i - 1][j - 1]) + (result[i - 1][j]))
+
+        return result[rowIndex]
+
+
+n = 6
+x = Solution()
+print(x.getRow(n))
