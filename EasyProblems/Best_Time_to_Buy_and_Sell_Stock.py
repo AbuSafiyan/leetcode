1af8dc54ce17343acfebc7daf5a1beece031eb30
AbuSafiyan <safiyanabu@gmail.com>
2022-06-03 18:06:10 +0530

    Modified Python file

--- EasyProblems/Best_Time_to_Buy_and_Sell_Stock.py
+++ EasyProblems/Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,14 +1,14 @@
 def maxProfit(prices):
-    l, r = 0, 1
-    max_Prof = 0
-    while len(prices) > r:
-        if prices[l] < prices[r]:
-            profit = prices[r] - prices[l]
+    left_Pointer, right_Pointer = 0, 1
+    max_Profit = 0
+    while len(prices) > right_Pointer:
+        if prices[left_Pointer] < prices[right_Pointer]:
+            profit = prices[right_Pointer] - prices[left_Pointer]
             max_Prof = max(max_Prof, profit)
         else:
-            l = r
-        r += 1
-    return max_Prof
+            left = right_Pointer
+        right_Pointer += 1
+    return max_Profit
 
 
 a = [7, 1, 3, 5, 6, 4]
