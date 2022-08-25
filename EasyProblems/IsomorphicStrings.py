f193f400a24f5c7faed2cd6abbb8203d4bc9e41c
AbuSafiyan <safiyanabu@gmail.com>
2022-08-25 20:17:20 +0530

    Modified IsomorphicStrings.py file

--- EasyProblems/IsomorphicStrings.py
+++ EasyProblems/IsomorphicStrings.py
@@ -1,22 +1,3 @@
-# IsomorphicStrings.py
-
-
-"""1- Consider a mapping table that maps each char from the first string to one and only char in the second string.
-2- Consider a mappedBefore table that record each char from the second string that is already related to a char
-   from the first string.
-3- Read characters of the first string one by one.
-4- If the read char from the first string is in the mapping table get its mapping.
-5- Read the related char from the second string.
-6- If the mapping and the read char from the second string are not the same return false.
-7- Else If the read char from the first string does not exist in the mapping table,
-	 *    1- read the related char from the second string,
-	 *    2- If the char exist in the mappedBefore table, return false.
-	 *    3- Else, add the read char from the first table and the read char from the second table to the
-	      mapping table.
-8- Go to 3- and continue.
-9 - Return true."""
-
-
 def isIsomorphic(s, t):
     map1 = {}
     map2 = {}
@@ -31,4 +12,4 @@ def isIsomorphic(s, t):
     return True
 
 
-print(isIsomorphic('paper', 'title'))
+print(isIsomorphic('egg', 'add'))
