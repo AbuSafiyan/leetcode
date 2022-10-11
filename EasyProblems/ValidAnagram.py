c35174f0c3f161c23e55fd811634dea005e271ee
AbuSafiyan <safiyanabu@gmail.com>
2022-10-11 18:48:23 +0530

    Added ValidAnagram.py file

---
+++ EasyProblems/ValidAnagram.py
@@ -0,0 +1,12 @@
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    elif sorted(s) == sorted(t):
+        return True
+
+    else:
+        return False
+
+
+print(isAnagram('heart', 'earth'))
