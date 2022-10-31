21cac5ecf0402852311cfbe8fc57be48cbbb40e3
AbuSafiyan <safiyanabu@gmail.com>
2022-10-31 20:46:50 +0530

    Added KeyboardRow.py file.

---
+++ EasyProblems/KeyboardRow.py
@@ -0,0 +1,30 @@
+# Given an array of strings words, return the words that can be typed using letters of the alphabet on
+# only one row of American keyboard like the image below.
+# In the American keyboard:
+# the first row consists of the characters "qwertyuiop",
+# the second row consists of the characters "asdfghjkl", and
+# the third row consists of the characters "zxcvbnm".
+
+
+def findWords(words):
+    st = {'q': 1, 'w': 1, 'e': 1, 'r': 1, 't': 1, 'y': 1, 'u': 1, 'i': 1, 'o': 1, 'p': 1, 'a': 2, 's': 2, 'd': 2,
+          'f': 2, 'g': 2, 'h': 2, 'j': 2, 'k': 2, 'l': 2, 'z': 3, 'x': 3, 'c': 3, 'v': 3, 'b': 3, 'n': 3, 'm': 3}
+
+    ret = []
+
+    for wd in words:
+        val = 0
+        for i in range(len(wd)):
+            if i == 0:
+                val = st.get(wd[i].lower())
+            else:
+                if val != st.get(wd[i].lower()):
+                    val = -1
+                    break
+        if val != -1:
+            ret.append(wd)
+    return ret
+
+
+a = ["Hello", "Alaska", "Dad", "Peace"]
+print(findWords(a))
