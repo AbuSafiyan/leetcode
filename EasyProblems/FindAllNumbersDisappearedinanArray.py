3864dabd51c67b02f1866e803be645701bb5caeb
AbuSafiyan <safiyanabu@gmail.com>
2022-11-20 13:44:04 +0530

    Added FindAllNumbersDisappearedinanArray.py file

---
+++ EasyProblems/FindAllNumbersDisappearedinanArray.py
@@ -0,0 +1,14 @@
+# Given an array nums of n integers where nums[i] is in the range [1, n],
+# return an array of all the integers in the range [1, n] that do not appear in nums.
+
+
+def findDisappearedNumbers(nums):
+    num = len(nums)
+    su = sum(nums)
+    num1 = (num * (num + 1)) // 2 - su
+    return num1
+
+
+a = [4, 3, 2, 7, 8, 2, 3, 1]
+print(findDisappearedNumbers(a))
+
